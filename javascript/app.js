afeaf6bdbe9194930ebdb022cec2ecbc46b662d1
naveen <neonew8984@gmail.com>
2021-05-18 17:31:09 +0530

    validateName function complete

--- javascript/app.js
+++ javascript/app.js
@@ -4,7 +4,15 @@ document.getElementById('zip').addEventListener('blur', validateZip);
 document.getElementById('email').addEventListener('blur', validateEmail);
 document.getElementById('phone').addEventListener('blur', validatePhone);
 
-function validateName() {}
+function validateName() {
+    const name = document.getElementById('name');
+    const re = /^[a-zA-Z]{2,10}$/;
+    if (!re.test(name.value)) {
+        name.classList.add('is-invalid');
+    } else {
+        name.classList.remove('is-invalid');
+    }
+}
 
 function validateZip() {}
 
